b07bfbdfa67dce46980b9152a3aaa4a149b5c432
Lilian Marzet <lilian.marzet123@gmail.com>
2023-08-12 02:25:50 +0200

    [FEAT] Mise à jour du theme

--- src/resources/assets/theme/CustomTheme.tsx
+++ src/resources/assets/theme/CustomTheme.tsx
@@ -5,26 +5,16 @@ export interface CustomTheme extends Theme {
     colors: Theme['colors'] & {
         primary: string;
         onPrimary: string;
-        primaryContainer: string;
-        onPrimaryContainer: string;
         secondary: string;
         onSecondary: string;
-        secondaryContainer: string;
-        onSecondaryContainer: string;
         tertiary: string;
         onTertiary: string;
-        tertiaryContainer: string;
-        onTertiaryContainer: string;
         error: string;
         onError: string;
         errorContainer: string;
         onErrorContainer: string;
         background: string;
         onBackground: string;
-        surface: string;
-        onSurface: string;
-        surfaceVariant: string;
-        onSurfaceVariant: string;
     };
 }
 declare module '@react-navigation/native' {
